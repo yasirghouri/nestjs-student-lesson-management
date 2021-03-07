3d656961ec01d2720301663197656e74fbfaaadb
yasirghouri <yasir.ag7@gmail.com>
2021-03-07 19:45:32 +0500

    added createLesson mutation

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,15 +1,23 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver(() => LessonType)
 export class LessonResolver {
-  @Query(() => LessonType)
-  lesson() {
-    return {
-      id: '1',
-      name: 'Physics class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  constructor(private lessonService: LessonService) {}
+
+  //   @Query(() => LessonType)
+  //   lesson(@Args('id') id: string) {
+  //     return;
+  //   }
+
+  @Mutation(() => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
