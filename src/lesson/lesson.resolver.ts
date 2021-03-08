1542d7d253ea94c55e8f5566396b152b9d729aea
yasirghouri <yasir.ag7@gmail.com>
2021-03-08 11:44:06 +0500

    added Student module to typeorm and added student entity

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,5 +1,4 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { Lesson } from './lesson.entity';
 import { CreateLessonInput } from './lesson.input';
 
 import { LessonService } from './lesson.service';
