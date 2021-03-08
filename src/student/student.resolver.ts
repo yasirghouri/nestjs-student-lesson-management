98aaeb802b08c5ad4127c78c0061caf049508747
yasirghouri <yasir.ag7@gmail.com>
2021-03-08 12:06:19 +0500

    added mutation to create a student

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateStudentInput } from './create-student.input';
+import { Student } from './student.entity';
+import { StudentService } from './student.service';
+import { StudentType } from './student.type';
 
-@Resolver()
-export class StudentResolver {}
+@Resolver(() => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Mutation(() => StudentType)
+  async createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput);
+  }
+}
