34f66b6e916c6d20ba73879e9051e0440674bbeb
yasirghouri <yasir.ag7@gmail.com>
2021-03-08 11:26:43 +0500

    added Validation to our input for graphql

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,4 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { create } from 'node:domain';
+import { CreateLessonInput } from './lesson.input';
 
 import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
@@ -14,10 +16,8 @@ export class LessonResolver {
 
   @Mutation(() => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ) {
-    return this.lessonService.createLesson(name, startDate, endDate);
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
 import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -16,11 +17,8 @@ export class LessonService {
     return this.lessonRepository.findOne({ id });
   }
 
-  async createLesson(
-    name: string,
-    startDate: string,
-    endDate: string,
-  ): Promise<Lesson> {
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
